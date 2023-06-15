c27f9a2410bcb37dfb31ad010c829c70385d776a
ericonobre <ericons@gmail.com>
2023-06-15 14:43:21 -0300

    dia 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <div className="app__videos">
         <Video />
+        <Video />
+        <Video />
+        <Video />
       </div>
     </div>
   );

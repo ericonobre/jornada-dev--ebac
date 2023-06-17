74621aac683bdb013527f39a09f3e636fe196101
ericonobre <ericons@gmail.com>
2023-06-17 14:30:03 -0300

    dia 4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={637}
+          messages={359}
+          shares={298}
+          name="erico.ns"
+          description="Gato pulando"
+          music="Jason M Norwood - Talking in Isolation, Pt. 4"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video 
+          likes={432}
+          messages={321}
+          shares={212}
+          name="erico.ns"
+          description="Gato cheirando"
+          music="Clap your Hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
+        
       </div>
     </div>
   );
